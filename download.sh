a74ed96de61518ad88b582b09ad930e8372c98bf
emkor93 <emkor93@gmail.com>
2019-05-24 16:18:18 +0200

    seems to be working after initial tests, so I added removal flag

--- download.sh
+++ download.sh
@@ -6,5 +6,5 @@ OUT_DIR=$1
 
 echo "Starting gmail-dl..."
 source ~/cam/reolink-gmail-archival/creds.sh
-gmail-dl ${OUT_DIR} --days 2 --log ~/cam/log/gmail-dl.log
+gmail-dl ${OUT_DIR} --days 2 --log ~/cam/log/gmail-dl.log --rm
 echo "Done!"
