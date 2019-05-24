646c446c2688ea10f469970ce4e4ab19c79b189b
emkor93 <emkor93@gmail.com>
2019-05-24 14:03:17 +0200

    fixed setup typo, added simple readme

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # reolink-gmail-archival
+Set of scripts that archives and uploads Reolink camera pictures to B2 
--- setup.sh
+++ setup.sh
@@ -6,5 +6,5 @@ set -e
 mkdir -p ~/cam/log && mkdir -p ~/cam/data && cd ~/cam
 
 git clone https://github.com/emkor/gmail-img-dl.git && cd ~/cam/gmail-img-dl
-sudo pip install- e ".[dev]"
+sudo pip install -e ".[dev]"
 gmail-dl --help
