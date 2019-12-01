530765438835f1f2e885c09a7522047753b3d539
emkor93 <emkor93@gmail.com>
2019-12-01 10:25:49 +0100

    switch run object detection scripts so it uses docker images

--- run_obj_detection.sh
+++ run_obj_detection.sh
@@ -5,23 +5,32 @@ set -e
 IMG_DIR=$1
 RESULTS_DIR=$2
 RENDER_DIR=$3
-MODEL_FILE=$4
-CLASSES_FILE=$5
 
-source ~/recogn-img/.venv/recogn-img/bin/activate
+mkdir -p tmp_image
+mkdir -p tmp_render
+mkdir -p tmp_result
 
-for d in $(find $IMG_DIR -type d | grep "20" | sort)
+for d in $(find $IMG_DIR/* -type d | sort)
 do
-  RESULTS_FILE="$RESULTS_DIR/"$(basename $d)".json"
+  RESULT_PATH="$RESULTS_DIR/"$(basename $d)
   FILTERED_RESULTS_FILE="$RESULTS_DIR/"$(basename $d)"_filter.json"
   RENDER_PATH="$RENDER_DIR/$(basename $d)"
   IMG_COUNT_BEFORE=$(find $d -type f -name "*.jpg" | wc -l)
-  echo "Starting object detection job for $d ($IMG_COUNT_BEFORE images) with output path of $RENDER_PATH"
+  mv $d/* tmp_image
   mkdir -p $RENDER_PATH
-  recogn-img $MODEL_FILE $CLASSES_FILE $d $RESULTS_FILE --box-threshold 0.4 --obj-threshold 0.5
-  result-filter $RESULTS_FILE $FILTERED_RESULTS_FILE --whitelist car,truck,train,motorbike,bus,boat,aeroplane,bicycle,person,dog,cow,horse,cat,sheep,bear,elephant,zebra,bird,giraffe
-  render-recogn --copy-exif $FILTERED_RESULTS_FILE $RENDER_PATH
+  mkdir -p $RESULT_PATH
+  docker-compose down
+  echo "Starting object detection job for $d ($IMG_COUNT_BEFORE images) with output path of $RENDER_PATH"
+  docker-compose up --no-color recogn
+  docker-compose up --no-color filter
+  docker-compose up --no-color render
+  mv tmp_image/* $d
+  mv tmp_result/* $RESULT_PATH
+  for ri in $(find tmp_render -type f -name "*.jpg"); do mv $ri $RENDER_PATH/ ; done
   IMG_COUNT_AFTER=$(find $RENDER_PATH -type f -name "*.jpg" | wc -l)
   echo "Done job for $d, $IMG_COUNT_BEFORE -> $IMG_COUNT_AFTER"
 done
-py
+
+docker-compose down
+
+# nohup ~/run_obj_detection.sh ~/dl_img ~/result ~/render >> ~/obj_detection.log 2>&1 &
